d79ff562e29203516ac9f98b286bc8bc75908a74
Bennett S. <bennett.scarff@gmail.com>
2026-05-23 20:18:49 -0700

    Add comment about new C++ file standard
    Added a comment explaining the new file standard for C++ and its functionality.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 
 // A new file standard for the C++ language.
+//replaces EMBED precomiler with a function in the language that accepts STATIC as a type of file to embed at compile time.
 
 struct file {
 	unsigned int INTERNAL_ID;
